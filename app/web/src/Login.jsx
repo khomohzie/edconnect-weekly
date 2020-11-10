7e95f9245d85807622f6388ff990b49ac6f801fc
Daniel Komolafe <komozy2000@gmail.com>
2020-11-10 08:23:44 +0100

    Completed Week 7 Exercise

--- app/web/src/Login.jsx
+++ app/web/src/Login.jsx
@@ -44,6 +44,10 @@ const Login = (props) => {
                     var err = await res.json();
 
                     setError("Invalid email/password");
+
+                    console.log(err);
+
+                    return err;
                 }
             })
     }
@@ -53,29 +57,29 @@ const Login = (props) => {
 
             <main role="main">
 
-                <div class="container">
+                <div className="container">
 
-                    <div class="row mt-3">
-                        <div class="col-12 col-md-8 offset-md-2">
+                    <div className="row mt-3">
+                        <div className="col-12 col-md-8 offset-md-2">
                             <h1>Login</h1>
 
                             { error ? <Alert variant="danger"> { error } </Alert> : null }
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-12 col-md-8 offset-md-2">
+                    <div className="row">
+                        <div className="col-12 col-md-8 offset-md-2">
                             <form onSubmit={onLogin} encType="text/plain" id="loginForm">
-                                <fieldset class="form-group">
-                                    <label for="email" class="col-form-label">Email address</label>
-                                    <input class="form-control" type="email" onChange={handleInput} name="email" value={login.email} maxlength="50" placeholder="Enter email" />
+                                <fieldset className="form-group">
+                                    <label htmlFor="email" className="col-form-label">Email address</label>
+                                    <input className="form-control" type="email" onChange={handleInput} name="email" value={login.email || ""} maxLength="50" placeholder="Enter email" />
                                 </fieldset>
-                                <fieldset class="form-group">
-                                    <label for="password" class="col-form-label">Password</label>
-                                    <input class="form-control" type="password" onChange={handleInput} name="password" value={login.password} placeholder="Password" />
+                                <fieldset className="form-group">
+                                    <label htmlFor="password" className="col-form-label">Password</label>
+                                    <input className="form-control" type="password" onChange={handleInput} name="password" value={login.password || ""} placeholder="Password" />
                                 </fieldset>
 
-                                <input class="btn btn-primary" type="submit" value="Login" />
+                                <input className="btn btn-primary" type="submit" value="Login" />
                             </form>
                         </div>
                     </div>
